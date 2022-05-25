aabc2212897a92a44bbdb66ead19f4e8364e76d3
geethanjali-2 <100390368+geethanjali-2@users.noreply.github.com>
2022-05-25 14:25:01 +0530

    Initial commit

--- src/main/java/com/eproject/usecase/controller/StudentController.java
+++ src/main/java/com/eproject/usecase/controller/StudentController.java
@@ -1,10 +1,14 @@
 package com.eproject.usecase.controller;
 
+import com.eproject.usecase.model.Student;
 import com.eproject.usecase.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -16,5 +20,30 @@ public class StudentController {
         model.addAttribute("listStudent",studentService.getAllStudents());
         return "index";
     }
+    @GetMapping("/showNewStudentForm")
+    public String showNewStudentForm(Model model){
+        Student student=new Student();
+        model.addAttribute("student",student);
+        return  "new_student";
+
+    }
+    @PostMapping("/saveStudent")
+    public String saveStudent(@ModelAttribute("student") Student student){
+        studentService.saveStudent(student);
+        return "redirect:/";
+    }
+    @GetMapping("/showFormForUpdate/{id}")
+    public String showFormForUpdate(@PathVariable(value = "id") long id,Model model){
+        Student student=studentService.getStudentById(id);
+        model.addAttribute("student",student);
+        return "update_student";
+
+    }
+    @GetMapping("/deleteStudent/{id}")
+    public String deleteStudent(@PathVariable(value = "id") long id){
+        this.studentService.deleteStudentById(id);
+        return "redirect:/";
+    }
+
 
 }
--- src/main/java/com/eproject/usecase/service/StudentService.java
+++ src/main/java/com/eproject/usecase/service/StudentService.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface StudentService {
     List<Student> getAllStudents();
     void saveStudent(Student student);
+    Student getStudentById(long id);
+    void deleteStudentById(long id);
 }
